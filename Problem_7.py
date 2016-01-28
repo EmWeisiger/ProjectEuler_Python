53b2c4f3d17945b18bbd6897b14e15978c30652e
Em Weisiger <emweisiger@gmail.com>
2016-01-28 13:51:55 -0700

    just the printing prime numbers part

--- Problem_7.py
+++ Problem_7.py
@@ -1,3 +1,13 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
 # What is the 10 001st prime number?
+
+
+#Just printing prime numbers part
+for n in range(10001):
+    prime = True
+    for i in range(2, n):
+        if (n % i == 0):
+            prime = False
+    if prime:
+        print n
