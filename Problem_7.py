16e5129cf78d20b17a795c06cb7f75215fbb103e
Em Weisiger <emweisiger@gmail.com>
2016-01-28 16:43:05 -0700

    Update Problem_7.py

--- Problem_7.py
+++ Problem_7.py
@@ -2,16 +2,31 @@
 
 # What is the 10 001st prime number?
 
-primeList = []
-for n in range(22000000):
-    prime = True
-    n = n + 1
-    for i in range(2, n):
-        if (n % i == 0):
-            prime = False
-    if prime:
-        primeList.append(n)
+# primeList = []
+# for n in range(22000000):
+#     prime = True
+#     n = n + 1
+#     for i in range(2, n):
+#         if (n % i == 0):
+#             prime = False
+#     if prime:
+#         primeList.append(n)
 
 # printing the length of the list to determine how close I am the to 10 001st
 # print len(primeList)
-print primeList[10001]
+# print primeList[10001]
+
+# different approach that is more direct and less time consuming
+stop = 10001
+count = 1
+prime = 1
+
+while count < stop:
+    prime += 2
+    is_prime = True
+    for i in range(2,n):
+        if(prime % i == 0):
+            is_prime = False
+    if is_prime:
+        count + 1
+print prime
