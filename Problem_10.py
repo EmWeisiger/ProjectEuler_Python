c867659960f09ae83890e350c3cb02fe5d8dcb13
Em Weisiger <emweisiger@gmail.com>
2016-02-05 10:21:10 -0700

    Basic structure but w/ timeouts

--- Problem_10.py
+++ Problem_10.py
@@ -1,3 +1,19 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 # Find the sum of all the primes below two million.
+n = 1
+prime = 1
+stop 2000000
+total_sum = 0
+
+while n < stop:
+    prime += 2
+    is_prime = True
+    for i in range(2,n):
+        if(prime % i == 0):
+            is_prime = False
+    if is_prime:
+        total_sum = total_sum + prime
+print total sum
+
+# timeouts when testing
